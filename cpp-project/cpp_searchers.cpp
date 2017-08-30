f3aebb9411ea3244bbfa304db8debf8e73dcc6ae
Bartłomiej Filipek <joebaf@gmail.com>
2017-08-30 22:24:12 +0200

    working searcher

--- cpp-project/cpp_searchers.cpp
+++ cpp-project/cpp_searchers.cpp
@@ -1,50 +1,22 @@
-#include <string>
 #include <iostream>
-#include <any>
-#include <map>
+#include <string>
+#include <algorithm>
+#include <functional>
+// in gcc 7.1 still have to use experimental namespace/headers
+#include <experimental/functional>
+#include <experimental/algorithm>
  
 int main()
 {
-    auto a = std::any(12);
-    
-    // set any value:
-    a = std::string("Hello!");
-    a = 16;
-    // reading a value:
-    
-    // we can read it as int
-    std::cout << std::any_cast<int>(a) << '\n'; 
- 
-    // but not as string:
-    try 
-    {
-        std::cout << std::any_cast<std::string>(a) << '\n';
-    }
-    catch(const std::bad_any_cast& e) 
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-    // reset and check if it contains any value:
-    a.reset();
-    if (!a.has_value())
-    {
-        std::cout << "a is empty!" << "\n";
-    }
-    
-    // you can use it in a container:
-    std::map<std::string, std::any> m;
-    m["integer"] = 10;
-    m["string"] = std::string("Hello World");
-    m["float"] = 1.0f;
-    
-    for (auto &[key, val] : m)
-    {
-        if (val.type() == typeid(int))
-            std::cout << "int: " << std::any_cast<int>(val) << "\n";
-        else if (val.type() == typeid(std::string))
-            std::cout << "string: " << std::any_cast<std::string>(val) << "\n";
-        else if (val.type() == typeid(float))
-            std::cout << "float: " << std::any_cast<float>(val) << "\n";
-    }
+    std::string in = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
+                     " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+    std::string needle = "pisci";
+    auto it = std::experimental::search(in.begin(), in.end(),
+                   std::experimental::boyer_moore_searcher(
+                       needle.begin(), needle.end()));
+    if(it != in.end())
+        std::cout << "The string " << needle << " found at offset "
+                  << it - in.begin() << '\n';
+    else
+        std::cout << "The string " << needle << " not found\n";
 }
