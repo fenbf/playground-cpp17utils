8c3c70dec67ef4667b38761caccccbacb3d10fc5
Bartłomiej Filipek <joebaf@gmail.com>
2017-08-30 23:32:41 +0200

    searcher update, with benchmark

--- cpp-project/cpp_searchers.cpp
+++ cpp-project/cpp_searchers.cpp
@@ -5,18 +5,53 @@
 // in gcc 7.1 still have to use experimental namespace/headers
 #include <experimental/functional>
 #include <experimental/algorithm>
+#include <chrono>
+
+template <typename TFunc> void RunAndMeasure(TFunc func)
+{
+    const auto start = std::chrono::steady_clock::now();
+    func();
+    const auto end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration <double, std::milli> (end - start).count() << " ms\n";
+}
  
 int main()
 {
-    std::string in = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
-                     " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-    std::string needle = "pisci";
+    std::string in;
+    std::string needle = "Hello Amazing Programming World";
+    RunAndMeasure([&]() {
+        const int maxIters = 20000000u;
+        for (int i = 0; i < maxIters; ++i)
+        {
+            in += "abcd ";
+            if (i % 7)
+                in += "xyz, uvw ";
+            if (i == maxIters - 100)
+                in += " $" + needle + "$ ";
+        }
+    });
+    
+    RunAndMeasure([&]() {
+    auto it = std::experimental::search(in.begin(), in.end(),
+                   std::experimental::default_searcher(
+                       needle.begin(), needle.end()));
+    if(it == in.end())
+        std::cout << "The string " << needle << " not found\n";
+    });
+    
+    RunAndMeasure([&]() {
     auto it = std::experimental::search(in.begin(), in.end(),
                    std::experimental::boyer_moore_searcher(
                        needle.begin(), needle.end()));
-    if(it != in.end())
-        std::cout << "The string " << needle << " found at offset "
-                  << it - in.begin() << '\n';
-    else
+    if(it == in.end())
         std::cout << "The string " << needle << " not found\n";
+    });
+    
+    RunAndMeasure([&]() {
+    auto it = std::experimental::search(in.begin(), in.end(),
+                   std::experimental::boyer_moore_horspool_searcher(
+                       needle.begin(), needle.end()));
+    if(it == in.end())
+        std::cout << "The string " << needle << " not found\n";
+    });
 }
