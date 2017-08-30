651685813f3e5bdbeef2015b59ef2293e655aca3
Bartłomiej Filipek <joebaf@gmail.com>
2017-08-30 13:45:33 +0200

    improved code sample for any

--- cpp-project/cpp_any.cpp
+++ cpp-project/cpp_any.cpp
@@ -5,13 +5,29 @@
 int main()
 {
     auto a = std::any(12);
- 
+    
+    // set any value:
+    a = std::string("Hello!");
+    a = 16;
+    // reading a value:
+    
+    // we can read it as int
     std::cout << std::any_cast<int>(a) << '\n'; 
  
-    try {
+    // but not as string:
+    try 
+    {
         std::cout << std::any_cast<std::string>(a) << '\n';
     }
-    catch(const std::bad_any_cast& e) {
+    catch(const std::bad_any_cast& e) 
+    {
         std::cout << e.what() << '\n';
+    }
+    
+    // reset and check if it contains any value:
+    a.reset();
+    if (!a.has_value())
+    {
+        std::cout << "a is empty!" << "\n";
     }
 }
