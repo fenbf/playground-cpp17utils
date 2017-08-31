aaadf26ce58880b59cfd3c950745454d64e06aa9
Bartłomiej Filipek <joebaf@gmail.com>
2017-08-31 23:20:34 +0200

    optional update

--- cpp-project/cpp_optional.cpp
+++ cpp-project/cpp_optional.cpp
@@ -1,50 +1,26 @@
-#include <string>
+#include <optional>
 #include <iostream>
-#include <any>
-#include <map>
+
+std::optional<int> GetInt(int r)
+{
+    if (r % 2 == 0)
+        return r/2;
+    
+    return { };
+}
+
+void ShowOptionalInt(const std::optional<int>& oi)
+{
+    if (oi)
+        std::cout << "int ok: " << *oi << "\n";
+    else
+        std::cout << "bad int...\n";
+}
  
 int main()
 {
-    auto a = std::any(12);
-    
-    // set any value:
-    a = std::string("Hello!");
-    a = 16;
-    // reading a value:
-    
-    // we can read it as int
-    std::cout << std::any_cast<int>(a) << '\n'; 
- 
-    // but not as string:
-    try 
-    {
-        std::cout << std::any_cast<std::string>(a) << '\n';
-    }
-    catch(const std::bad_any_cast& e) 
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-    // reset and check if it contains any value:
-    a.reset();
-    if (!a.has_value())
-    {
-        std::cout << "a is empty!" << "\n";
-    }
-    
-    // you can use it in a container:
-    std::map<std::string, std::any> m;
-    m["integer"] = 10;
-    m["string"] = std::string("Hello World");
-    m["float"] = 1.0f;
-    
-    for (auto &[key, val] : m)
-    {
-        if (val.type() == typeid(int))
-            std::cout << "int: " << std::any_cast<int>(val) << "\n";
-        else if (val.type() == typeid(std::string))
-            std::cout << "string: " << std::any_cast<std::string>(val) << "\n";
-        else if (val.type() == typeid(float))
-            std::cout << "float: " << std::any_cast<float>(val) << "\n";
-    }
+    auto oi = GetInt(10);
+    ShowOptionalInt(oi);
+    auto oi2 = GetInt(11);
+    ShowOptionalInt(oi2);
 }
