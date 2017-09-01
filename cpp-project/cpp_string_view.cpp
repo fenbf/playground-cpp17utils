743f512518b5174cf75852757611fdc11f7b75b9
Bartłomiej Filipek <joebaf@gmail.com>
2017-09-01 14:41:31 +0200

    string view sample

--- cpp-project/cpp_string_view.cpp
+++ cpp-project/cpp_string_view.cpp
@@ -1,50 +1,26 @@
 #include <string>
 #include <iostream>
-#include <any>
-#include <map>
+#include <string_view>
+
+// we need to overload 'new' to see what's
+// hapenning under the hood...
+void* operator new(std::size_t n)
+{
+    std::cout << "new " << n << " bytes\n";
+    return malloc(n);
+}
  
 int main()
 {
-    auto a = std::any(12);
+    // the original string, one allocation:
+    std::string str {"Hello Amazing Programming World" };
     
-    // set any value:
-    a = std::string("Hello!");
-    a = 16;
-    // reading a value:
+    // another allocation for the substring, separate 'copy'
+    auto subStr = str.substr(str.find("Programming"));
+    std::cout << subStr << "\n";
     
-    // we can read it as int
-    std::cout << std::any_cast<int>(a) << '\n'; 
- 
-    // but not as string:
-    try 
-    {
-        std::cout << std::any_cast<std::string>(a) << '\n';
-    }
-    catch(const std::bad_any_cast& e) 
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-    // reset and check if it contains any value:
-    a.reset();
-    if (!a.has_value())
-    {
-        std::cout << "a is empty!" << "\n";
-    }
-    
-    // you can use it in a container:
-    std::map<std::string, std::any> m;
-    m["integer"] = 10;
-    m["string"] = std::string("Hello World");
-    m["float"] = 1.0f;
-    
-    for (auto &[key, val] : m)
-    {
-        if (val.type() == typeid(int))
-            std::cout << "int: " << std::any_cast<int>(val) << "\n";
-        else if (val.type() == typeid(std::string))
-            std::cout << "string: " << std::any_cast<std::string>(val) << "\n";
-        else if (val.type() == typeid(float))
-            std::cout << "float: " << std::any_cast<float>(val) << "\n";
-    }
+    // no allocation for the sub range:
+    std::string_view strView { str };
+    auto subView = strView.substr(str.find("Programming"));
+    std::cout << subView << "\n";
 }
